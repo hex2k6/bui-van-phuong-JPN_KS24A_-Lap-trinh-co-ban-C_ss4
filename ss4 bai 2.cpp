a03e4332549a2752c45bb17e8591d82846d36102
hex2k6 <hexgaming26@gmail.com>
2024-11-24 22:54:27 +0700

    upload BT2 S4

---
+++ ss4 bai 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	
+	int number;
+	printf("moi ban nhap 1 so nguyen");
+	scanf("%d",&number);
+	if(number % 2 ==0){
+		printf("so ban nhap la so chan");
+	}
+	else{
+		printf("so ban nhap la so le");
+	}
+
+	
+	return 0;
+	
+	
+}
+
